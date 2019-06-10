a239951fffdb036b9c51a12effe58ef3975b8b19
Jaclyn <jaclyndshahan@gmail.com>
2019-06-09 20:43:48 -0500

    else if/tryParsing

--- Program.cs
+++ Program.cs
@@ -8,23 +8,34 @@ namespace PracticeProject
 
         public static void Main(string[] args)
         {
+            Console.WriteLine("What's the temperature like today?");
             string temperature = Console.ReadLine();
             int numTemp = int.Parse(temperature);
+            int number;
 
+            if(int.TryParse(temperature, out number))
+            {
+                numTemp = number;
+            }
+            else
+            {
+                number = 0;
+                Console.WriteLine("value entered was no number. o set as temperature");
+            }
             if(numTemp < 20)
             {
                 Console.WriteLine("Take the coat");
-            }
-
-            if (numTemp == 20) 
+            } else if(numTemp == 20)
             {
                 Console.WriteLine("Pants and hoodie should be fine");
-            }
-            if(numTemp > 20)
+            } else if (numTemp > 30)
             {
-                Console.WriteLine("Shorts are enough today!");
+                Console.WriteLine("It's Hot");
             }
-
+            else
+            {
+                Console.WriteLine("Shorts are enough for today");
+            }
             Console.Read();
         }
     
