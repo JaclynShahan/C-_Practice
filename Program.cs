147de0ee6df58eee5a17616b4fd2464280426a24
Jaclyn <jaclyndshahan@gmail.com>
2019-06-09 19:56:14 -0500

    try/catch exceptions

--- Program.cs
+++ Program.cs
@@ -8,54 +8,22 @@ namespace PracticeProject
 
         public static void Main(string[] args)
         {
-           // string input = Console.ReadLine(); //one line of user input
-            Console.WriteLine(Calculate());
-            Console.Read();
-            //int result = Add(15, 31);
-           // Console.WriteLine(Multiply(25, 46));
-            //Console.WriteLine(Divide(100, 22));
-            //Console.Read();
-            //WriteSomething();
-            //WriteSomethingSpecific("I am an argument and am called from a method");
-            //Console.Read();
+         Console.WriteLine("Please write a number!");
+            string userInput = Console.ReadLine();
+            try
+            {
+                int userInputasInt = int.Parse(userInput);  
+            }
+            catch (FormatException)
+            {
+                 Console.WriteLine("Format Exception, please enter correct type next time");
+            } catch (Exception)
+            {
+                Console.WriteLine("General Exception. Something went wrong");
+            }
+
+            Console.ReadKey();
         }
-        public static int Calculate()
-        {   
-            Console.WriteLine("Please enter the first number");
-            string number1Input = Console.ReadLine();
-            Console.WriteLine("Please enter the second number");
-            string number2Input = Console.ReadLine();
-
-            int num1 = int.Parse(number1Input);
-            int num2 = int.Parse(number2Input);
-
-            int result = num1 + num2;
-            return result;
-        }
-        //public static int Add(int num1, int num2)
-        //{
-        //    return num1 + num2; 
-        //}
-
-        //public static int Multiply(int num1, int num2)
-        //{
-        //    return num1 * num2;
-        //}
-
-        //public static double Divide (double num1, double num2)
-        //{
-        //    return num1 / num2;
-        //}
-        ////access modifier (static) return type method name (param1, param2)
-        //public static void WriteSomething()
-        //{
-        //    Console.WriteLine("I am called from a method");
-        //}
-
-        //public static void WriteSomethingSpecific(string myText)
-        //{
-        //    Console.WriteLine(myText);
-           
-        //}
+    
     }
 }
