1d3bc9f394a820bb9c0e76b4681683a8a290451d
Jaclyn <jaclyndshahan@gmail.com>
2019-06-09 21:27:43 -0500

    break and continue

--- Program.cs
+++ Program.cs
@@ -8,47 +8,23 @@ namespace PracticeProject
 
         public static void Main(string[] args)
         {
-            //for loop
-            //    for (int counter = 1; counter < 20; counter += 2)
-            //    {
-            //        Console.WriteLine(counter);
-
-            //    }
-            //    Console.Read();
-            //}
-            //    for (int counter = 0; counter < 50; counter+=5)
-            //    {
-            //        Console.WriteLine(counter);
-            //    }
-            //       Console.WriteLine("For loop is done.");
-            //    Console.Read();
-            //}
-            //do While loop
-
-            //int lengthOfText = 0;
-            //string wholeText = "";
-
-            //do
+            //for (int counter = 0; counter < 10; counter++)
             //{
-            //    Console.WriteLine("Please enter the name of a friend");
-            //    string nameOfAFriend = Console.ReadLine();
-            //    int currentLength = nameOfAFriend.Length;
-            //    lengthOfText += currentLength;
-            //    wholeText += nameOfAFriend;
-
+            //    Console.WriteLine(counter);
+            //    if(counter == 3)
+            //    {
+            //        Console.WriteLine("at 3 we stop");
+            //        break;
+            //    }
             //}
-            //while (lengthOfText < 20);
-            //Console.WriteLine("Thanks, that was enough");
-            //Console.Read();
-            int counter = 0;
-            string enteredText = "";
-            while(enteredText.Equals(""))
+            for (int counter = 0; counter < 10; counter++)
             {
-                Console.WriteLine("Please press enter to increase amount by one and anything else" + 
-                                 " enter if you want to finish counting");
-                enteredText = Console.ReadLine();
-                counter++;
-                Console.WriteLine("{0} people are inside the bus", counter);
+                Console.WriteLine(counter);
+                if (counter %2 == 0)
+                {
+                    Console.WriteLine("Here comes an odd number");
+                    continue;
+                }
             }
             Console.Read();
         }
