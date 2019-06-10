ccaaca427ef84f513a43057e8e825e44b24a0209
Jaclyn <jaclyndshahan@gmail.com>
2019-06-09 20:15:25 -0500

    operators

--- Program.cs
+++ Program.cs
@@ -8,19 +8,63 @@ namespace PracticeProject
 
         public static void Main(string[] args)
         {
-         Console.WriteLine("Please write a number!");
-            string userInput = Console.ReadLine();
-            try
-            {
-                int userInputasInt = int.Parse(userInput);  
-            }
-            catch (FormatException)
-            {
-                 Console.WriteLine("Format Exception, please enter correct type next time");
-            } catch (Exception)
-            {
-                Console.WriteLine("General Exception. Something went wrong");
-            }
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+            //unary operators
+            num3 = -num1;
+            Console.WriteLine("num3 is {0}",num3);
+
+            bool isSunny = true;
+            Console.WriteLine("is it sunny? {0}", isSunny);
+
+            //increment operators
+            int num = 0;
+            num++;
+            Console.WriteLine("num is {0}", num);
+            Console.WriteLine("num is {0}", num++);
+            //pre increment
+            Console.WriteLine("num is {0}", ++num);
+
+            //decrement operator
+            num--;
+
+            Console.WriteLine("num is {0}", num);
+            Console.WriteLine("num is {0}", num--);
+           // Pre decrement
+            Console.WriteLine("num is {0}", --num);
+
+            int result;
+            result = num1 + num2;
+            Console.WriteLine("result of num1 + num2 is {0}", result);
+            result = num1 * num2;
+            Console.WriteLine("result of num1 * num2 is {0}", result);
+            result = num1 / num2;
+            Console.WriteLine("result of num1 / num2 is {0}", result);
+            result = num1 % num2;
+            Console.WriteLine("result of num1 % num2 is {0}", result);
+
+            //relational and type operators
+            bool isLower;
+            isLower = num1 < num2;
+            Console.WriteLine("result of num1 < num2 is {0}", isLower);
+
+            //equality operator
+            bool isEqual;
+            isEqual = num1 == num2;
+            Console.WriteLine("result of num1 == num2 is {0}", isEqual);
+
+            isEqual = num1 != num2;
+            Console.WriteLine("result of num1 != num2 is {0}", isEqual);
+
+            //conditional operators
+            bool isLowerAndSunny = isLower && isSunny;
+            Console.WriteLine("result of isLower && isSunny is {0}", isLowerAndSunny);
+
+
+            isLowerAndSunny = isLower || isSunny;
+            Console.WriteLine("result of isLower || isSunny is {0}", isLowerAndSunny);
 
             Console.ReadKey();
         }
